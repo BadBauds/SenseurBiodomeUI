851e8151c317d5004f90b95e0a78592bb98b09fb
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2013-12-06 10:32:48 -0500

    v. 2a 4 boutons, fonction lecture

--- SenseurBiodomeUI.py
+++ SenseurBiodomeUI.py
@@ -18,14 +18,11 @@ def eventloop():
     if event["name"]=="click":
       id=event["data"]["id"]
       if id=="button1":
-	toSend = "#st" + droid.fullQueryDetail("editText1").result['text']
-        droid.bluetoothWrite(toSend)
+        droid.bluetoothWrite("#ga")
 	time.sleep(0.5)
-        readSerial()
+        readAll()
       elif id=="button2":
-        droid.bluetoothWrite("#gt")
-	time.sleep(0.5)
-        readSerial()
+        valider()
       elif id=="button3":
         droid.bluetoothWrite("#gd")
 	time.sleep(0.5)
@@ -72,75 +69,88 @@ def eventloop():
 
 print "Started"
 layout="""<?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-        android:id="@+id/background"
-        android:orientation="vertical" android:layout_width="match_parent"
-        android:layout_height="match_parent" android:background="#ff008080">
-        <LinearLayout android:layout_width="match_parent"
-                 android:id="@+id/linearLayout1">
-        <EditText android:layout_width="match_parent"
-                 android:layout_width="240dp" android:id="@+id/editText1"
-                android:tag="Tag Me" android:hint="Valeur" android:inputType="textCapWords|textPhonetic|number">
-                <requestFocus></requestFocus>
-        </EditText>
-                <Button android:id="@+id/button1"  android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Ajuster distance train"></Button>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android" android:id="@+id/background" android:orientation="vertical" android:layout_width="match_parent" android:layout_height="match_parent" android:background="#ff008080">
+    <LinearLayout android:layout_width="match_parent" android:layout_marginBottom="8dip" android:layout_marginLeft="10dip" android:layout_marginTop="15dip" android:id="@+id/linearLayout1">
+        <TextView android:layout_width="wrap_content" android:layout_height="wrap_content" android:text="Senseur DMX" android:id="@+id/textViewTitre" android:textStyle="bold" android:textSize="10sp" android:textColor="#000" android:gravity="center_vertical|center_horizontal|center"></TextView>
+        <ToggleButton android:id="@+id/togglebutton1" android:layout_width="wrap_content" android:layout_height="wrap_content" android:textOn="Senseur" android:textOff="Senseur" android:checked="true" android:layout_marginLeft="148dip" android:layout_width="74dip" android:gravity="center_vertical|center_horizontal|center" android:onClick="onToggleClicked"></ToggleButton>
+    </LinearLayout>
+<View
+    android:layout_width="fill_parent"
+    android:layout_height="1dp"
+    android:background="@android:color/darker_gray"/>
+    <LinearLayout android:layout_width="wrap_content" android:layout_marginLeft="10dip" android:layout_marginTop="8dip" android:id="@+id/linearLayout4">
+        <LinearLayout android:layout_width="match_parent" android:layout_height="match_parent" android:orientation="vertical" android:id="@+id/linearLayout2">
+            <TextView android:layout_width="wrap_content" android:layout_height="46dip" android:text="Distance de la cible: " android:id="@+id/textView1" android:textAppearance="?android:attr/textAppearanceLarge" android:gravity="center_vertical|center_horizontal|center"></TextView>
+            <TextView android:layout_width="wrap_content" android:layout_height="46dip" android:text="Delai de passage de la cible : " android:id="@+id/textView2" android:textAppearance="?android:attr/textAppearanceLarge" android:gravity="center_vertical|center_horizontal|center"></TextView>
+            <TextView android:layout_width="wrap_content" android:layout_height="46dip" android:text="Duree du sigal DMX : " android:id="@+id/textView3" android:textAppearance="?android:attr/textAppearanceLarge" android:gravity="center_vertical|center_horizontal|center"></TextView>
+            <TextView android:layout_width="wrap_content" android:layout_height="46dip" android:text="Marge de detection : " android:id="@+id/textView4" android:textAppearance="?android:attr/textAppearanceLarge" android:gravity="center_vertical|center_horizontal|center"></TextView>
         </LinearLayout>
-        <LinearLayout android:layout_width="match_parent"
-                 android:id="@+id/linearLayout1">
-                <Button android:id="@+id/button2" android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Voir distance train"></Button>
-                <Button android:id="@+id/button3"  android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Voir distance percu"></Button>
-                <Button android:id="@+id/button4" android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Voir temps de marche"></Button>
+        <LinearLayout android:layout_width="wrap_content" android:orientation="vertical" android:layout_marginLeft="87dip" android:id="@+id/linearLayout3">
+            <EditText android:id="@+id/editText1" android:tag="Tag Me" android:hint="Valeur" android:inputType="textCapWords|textPhonetic|number"></EditText>
+            <EditText android:id="@+id/editText2" android:tag="Tag Me" android:hint="Valeur" android:inputType="textCapWords|textPhonetic|number"></EditText>
+            <EditText android:id="@+id/editText3" android:tag="Tag Me" android:hint="Valeur" android:inputType="textCapWords|textPhonetic|number"></EditText>
+           <EditText android:id="@+id/editText4" android:tag="Tag Me" android:hint="Valeur" android:inputType="textCapWords|textPhonetic|number"></EditText>
         </LinearLayout>
-     <LinearLayout android:layout_width="match_parent"
-                 android:id="@+id/linearLayout1">
-                <Button android:id="@+id/button5" android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Partir DMX"></Button>
-                <Button android:id="@+id/button6"  android:layout_width="120dp" android:layout_height="70dp" android:layout_gravity="center"
-                         android:text="Ajuster temps train"></Button>
-                <Button android:id="@+id/button7" android:layout_width="120dp" android:layout_height="70dp" android:layout_gravity="center"
-                         android:text="Ajuster temps DMX"></Button>
-        </LinearLayout>
-     <LinearLayout android:layout_width="match_parent"
-                 android:id="@+id/linearLayout1">
-                <Button android:id="@+id/button8" android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Ajuster valeur superieure"></Button>
-                <Button android:id="@+id/button9"  android:layout_width="120dp" android:layout_height="70dp"
-                         android:text="Ajuster valeur inferieur"></Button>
-                <Button android:id="@+id/button10" android:layout_width="120dp" android:layout_height="70dp" android:layout_gravity="center"
-                         android:text="Activer/Desactiver senseur"></Button>
-        </LinearLayout>
-        <LinearLayout>
-        <EditText android:layout_width="match_parent"
-                 android:layout_width="120dp" android:hint="Resultat" android:id="@+id/editText2"
-                android:tag="Tag Me">
-                <requestFocus></requestFocus>
-        </EditText>
-        <ImageView
-                android:id="@+id/imageView1"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:layout_marginLeft="15dp"
-                android:src="file:///sdcard/badbauds.png">
-        </ImageView>
-</LinearLayout>
+    </LinearLayout>
+    <LinearLayout android:layout_width="match_parent" android:layout_marginLeft="245dip" android:layout_marginTop="10dip" android:id="@+id/linearLayout5">
+        <Button android:id="@+id/button1" android:layout_width="wrap_content" android:text="Lire"></Button>
+        <Button android:id="@+id/button2" android:layout_width="wrap_content" android:text="Valider"></Button>
+    </LinearLayout>
+    <ImageView android:id="@+id/imageView1" android:layout_width="wrap_content" android:layout_height="wrap_content" android:layout_marginLeft="15dip" android:src="file:///sdcard/badbauds.png"></ImageView>
 </LinearLayout>
 """
 
 
 def readSerial():
-  btRead = droid.bluetoothRead().result
+  btRead = droid.bluetoothReadLine().result
+  btRead = btRead[btRead.index('#'):]
   print btRead
   if btRead == 'error':
     droid.makeToast("Senseur hors de portee")
   else:
     droid.fullSetProperty("editText2","text", btRead)
 
+def getBetween(input, limiteGauche, limiteDroite=""):
+   if limiteDroite != "":
+      toDelete = input[input.index(limiteDroite):]
+      input = input.replace(toDelete, "")
+   input = input[input.index(limiteGauche):]
+   output = input.replace(limiteGauche, "")
+   return output
+
+def readAll():
+  btRead = droid.bluetoothReadLine().result
+  btRead = btRead[btRead.index('#'):]
+  results = parse(btRead)
+  droid.fullSetProperty("editText1","text", results["distance train"])
+  droid.fullSetProperty("editText2","text", results["temps train"])
+  droid.fullSetProperty("editText3","text", results["temps DMX"])
+  droid.fullSetProperty("editText4","text", results["range"])
+
+def parse(input):
+   results = {}
+   results["distance train"] = getBetween(input, "gt", "gd") + " cm"
+   results["distance"] = getBetween(input, "gd", "gr") + " cm"
+   results["running time"] = getBetween(input, "gr", "gtt") + " sec"
+   results["temps train"] = getBetween(input, "gtt", "gtd") + " sec"
+   results["temps DMX"] = getBetween(input, "gtd", "gra") + " sec"
+   results["range"] = getBetween(input, "gra", "as") + " cm"
+   return results
+
+def valider():
+   droid.bluetoothWrite("#st" + droid.fullQueryDetail("editText1").result['text'] + '0')
+   time.sleep(0.05)
+   droid.bluetoothWrite("#tt" + droid.fullQueryDetail("editText2").result['text'])
+   time.sleep(0.05)
+   droid.bluetoothWrite("#td" + droid.fullQueryDetail("editText3").result['text'])
+   time.sleep(0.05)
+   droid.bluetoothWrite("#sr" + droid.fullQueryDetail("editText4").result['text'] + '0')
+   time.sleep(0.05)
+   readSerial()
+
 print layout
 print droid.fullShow(layout)
+print droid.fullQueryDetail("linearLayout3").result
 eventloop()
 print droid.fullQuery()
 print "Data entered =",droid.fullQueryDetail("editText1").result
